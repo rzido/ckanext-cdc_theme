dd54f55d0cc2c9849f522cd12aea58fa9c41083e
rzido <rzido@yahoo.es>
2018-02-28 19:47:00 +0100

    Update plugin.py

--- ckanext/cdc_theme/plugin.py
+++ ckanext/cdc_theme/plugin.py
@@ -185,13 +185,13 @@ class CDCThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         unpublished_feedback_controller = 'ckanext.cdc_theme.controller:UnpublishedFeedbackController'
         unpublished_report_controller = 'ckanext.cdc_theme.controller:UnpublishedReportController'
 	additional_info_controller = 'ckanext.cdc_theme.controller:AdditionalInfoController'
-	cdc_showcase_controller = 'ckanext.cdc_theme.controller:CDC_Showcase
+	cdc_showcase_controller = 'ckanext.cdc_theme.controller:CDC_ShowcaseController
 
         map.connect('view_feedback', '/dataset/{id}/feedback', action='view_feedback',
                     controller=unpublished_feedback_controller)
         map.connect('view_org', '/unpublished_report/{org_id}', action='view_org',
                     controller=unpublished_report_controller)	
-	map.connect('read','/showcase/carousel_view/{id}', action='read', 
+	map.connect('read_carousel','/showcase/carousel_view/{id}', action='read_carousel, 
 		    controller=cdc_showcase_controller)
 	
 	map.connect('dataset_additional_info','/dataset/additional_info/{id}', action='additional_info', 
