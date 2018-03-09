55a0b093093c793b819117e352fefba7cd6b77c3
rzido <rzido@yahoo.es>
2018-03-09 12:27:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,32 +5,79 @@ version = '0.1'
 
 setup(
     name='ckanext-cdc_theme',
+    
+    # Versions should comply with PEP440.  For a discussion on single-sourcing  
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
     version=version,
+    
     description="",
     long_description='''
     ''',
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    
+     # What does your project relate to?
     keywords='',
+    
+    # Author details
     author='',
     author_email='',
+    
+    # The project's main homepage.
     url='',
+    
+    # Choose your license
     license='',
+    
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        # 3 - Alpha
+        # 4 - Beta
+        # 5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.        
+        'Programming Language :: Python :: 2.7',
+    ],
+    
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.cdc_theme'],
-    include_package_data=True,
-    zip_safe=False,
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires=[
         # -*- Extra requirements: -*-
     ],
+    
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    include_package_data=True,
+    package_data={
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
+
+    zip_safe=False,
+    
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
         # Add plugins here, e.g.
-        cdc_theme=ckanext.cdc_theme.plugin:CDCThemePlugin         
-        scheming_datasets=ckanext.scheming.plugins:SchemingDatasetsPlugin
-        scheming_groups=ckanext.scheming.plugins:SchemingGroupsPlugin
-        scheming_organizations=ckanext.scheming.plugins:SchemingOrganizationsPlugin        
-        [paste.paster_command]
-        scheming=ckanext.scheming.commands:SchemingCommand
+        cdc_theme=ckanext.cdc_theme.plugin:CDCThemePlugin                 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
     ''',
