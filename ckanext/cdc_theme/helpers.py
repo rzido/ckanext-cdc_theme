0795ecfd9bdace6875e7024f02ec1186fa4668e0
rzido <rzido@yahoo.es>
2018-04-13 11:28:36 +0200

    Update helpers.py

--- ckanext/cdc_theme/helpers.py
+++ ckanext/cdc_theme/helpers.py
@@ -8,30 +8,12 @@ import ckan.model as model
 from ckan.model.package import Package
 from ckan.lib.dictization.model_dictize import group_list_dictize
 import re
-import argparse
-from google.cloud import translate
-import six
+
 
 NotFound = logic.NotFound
 abort = base.abort
 
-def translate_text(target, text):
-    """Translates text into the target language.
-    Target must be an ISO 639-1 language code.
-    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
-    """
-    translate_client = translate.Client()
 
-    if isinstance(text, six.binary_type):
-        text = text.decode('utf-8')
-
-    # Text can also be a sequence of strings, in which case this method
-    # will return a sequence of results for each text.
-    result = translate_client.translate(
-        text, target_language=target)
-    
-    return(result['translatedText'])
-    
 
 def call_toolkit_function(fn, args, kwargs):
     return getattr(toolkit,fn)(*args, **kwargs)
