5b2b962e13759c1e08a7b0933c16b1b36144e229
rzido <rzido@yahoo.es>
2018-04-18 09:31:37 +0200

    Update plugin.py

--- ckanext/cdc_theme/plugin.py
+++ ckanext/cdc_theme/plugin.py
@@ -177,7 +177,7 @@ def showcase_count():
 def groups():
     """Return a list of groups"""
 
-    return tk.get_action('group_list')(data_dict={'all_fields': true, 'include_extras': true, 'include_dataset_count': true })
+    return tk.get_action('group_list')({},{'all_fields': True, 'include_extras': True, 'include_dataset_count': True })
 
 def collections():
     """Return a list of collections"""
